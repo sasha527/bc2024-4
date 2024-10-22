e79ca511c3e1c82d923d53e840b0bf9b059d8bf6
Olexander bc2024-4 <forostiansasha13@example.com>
2024-10-22 17:47:39 +0300

    зробив частину в якій реалізував метод GET

--- index.js
+++ index.js
@@ -1,27 +1,42 @@
+const fs = require('fs').promises;
 const http = require('http');
 const { Command } = require('commander');
+const path = require('path');
+
 const program = new Command();
 
 program
-  .requiredOption('-h, --host <address>', 'Address of the server')  // обов'язковий параметр
-  .requiredOption('-p, --port <number>', 'Port of the server')     // обов'язковий параметр
-  .requiredOption('-c, --cache <path>', 'Path to cache directory') // обов'язковий параметр
+  .requiredOption('-h, --host <address>', 'Address of the server')
+  .requiredOption('-p, --port <number>', 'Port of the server')
+  .requiredOption('-c, --cache <path>', 'Path to cache directory')
   .parse(process.argv);
 
 const options = program.opts();
 
-// Перевірка наявності обов'язкових параметрів
-if (!options.host || !options.port || !options.cache) {
-  console.error('Error: Missing required parameters');
-  process.exit(1);
-}
+const cacheFilePath = (statusCode) => path.join(options.cache, `${statusCode}.jpg`);
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello! This is a simple caching proxy server.');
-});
+const handleRequest = async (req, res) => {
+  const statusCode = req.url.slice(1); // Отримуємо код зі шляху, наприклад, /200
+  const filePath = cacheFilePath(statusCode);
+
+  
+    if (req.method === 'GET') {
+      // Отримання картинки
+      const data = await fs.readFile(filePath);
+      res.writeHead(200, { 'Content-Type': 'image/jpg' });
+      console.log(filePath); 
+      res.end(data);
+    } else {
+      // Метод не підтримується
+      res.writeHead(405, { 'Content-Type': 'text/plain' });
+      res.end('Method Not Allowed');
+    }
+
+};
+
+const server = http.createServer(handleRequest);
 
 server.listen(options.port, options.host, () => {
   console.log(`Server running at http://${options.host}:${options.port}/`);
-  console.log(`Cache directory is set to: ${options.cache}`);
 });
+
